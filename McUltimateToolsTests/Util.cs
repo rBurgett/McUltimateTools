bcc680ffa13edbaf4a93aeac80efb3f34f8625d0
rBurgett <ryan@burgettdev.net>
2024-02-04 01:06:49 -0500

    Changed regex patterns to constants

--- McUltimateToolsTests/Util.cs
+++ McUltimateToolsTests/Util.cs
@@ -13,14 +13,14 @@ public class UtilTests
     [Test]
     public void Util_GenerateUuid()
     {
-        var uuidPattern = @"[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}";
+        const string uuidPattern = @"[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}";
         var uuid = Util.GenerateUuid();
         Assert.That(Regex.IsMatch(uuid, uuidPattern), Is.True);
     }
     [Test]
     public void Util_GetDateString()
     {
-        var dateStringPattern = @"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z";
+        const string dateStringPattern = @"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z";
         var dateString = Util.GetDateString();
         Assert.That(Regex.IsMatch(dateString, dateStringPattern), Is.True);
     }
