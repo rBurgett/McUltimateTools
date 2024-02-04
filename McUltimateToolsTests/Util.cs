2db634362c692c25e9a51544f85b063cbe746ab0
rBurgett <ryan@burgettdev.net>
2024-02-04 01:01:01 -0500

    Added unit tests for Util

---
+++ McUltimateToolsTests/Util.cs
@@ -0,0 +1,27 @@
+using System.Text.RegularExpressions;
+using McUltimateTools;
+
+namespace McUltimateToolsTests;
+
+public class UtilTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Util_GenerateUuid()
+    {
+        var uuidPattern = @"[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}";
+        var uuid = Util.GenerateUuid();
+        Assert.That(Regex.IsMatch(uuid, uuidPattern), Is.True);
+    }
+    [Test]
+    public void Util_GetDateString()
+    {
+        var dateStringPattern = @"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z";
+        var dateString = Util.GetDateString();
+        Assert.That(Regex.IsMatch(dateString, dateStringPattern), Is.True);
+    }
+}
